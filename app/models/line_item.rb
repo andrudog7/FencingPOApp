7e330a15c0edf49ef6532a824421faf3aabd3e84
--add <andrewksmoker@gmail.com>
2021-04-20 16:35:41 -0400

    update line item total

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,9 +2,11 @@ class LineItem < ApplicationRecord
   belongs_to :purchase_order
   belongs_to :type
   belongs_to :item 
+  monetize :line_total_cents
 
   def calculate_total
     total = self.quantity * self.item.rate
-    self.update(line_total_cents: total)
+    byebug
+    self.update(line_total: total)
   end
 end
