deab7a526e2d7c58c8e26fa1137ff1bbda6f285c
--add <andrewksmoker@gmail.com>
2021-05-03 15:39:20 -0400

    add Customer search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,22 @@
 class CustomersController < ApplicationController
+    def index 
+        @customers = Customer.all
+        @customer = Customer.new 
+    end
+
+    def create
+        if params["commit"] === "Search"
+            @customers = Customer.where("name ~ ?", customer_params[:name])
+            @customer = Customer.new
+           
+            render 'index'
+        else
+            render 'index'
+        end
+    end
+
+    private 
+    def customer_params
+        params.require(:customer).permit(:name)
+    end
 end
