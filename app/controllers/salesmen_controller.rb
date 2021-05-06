9b3dcb9aa581e5c23b77a67534472689114ab571
--add <andrewksmoker@gmail.com>
2021-05-06 14:38:37 -0400

    Creating New Salesman form to add Image

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -2,4 +2,8 @@ class SalesmenController < ApplicationController
     def index 
         @salesmen = Salesman.all
     end
+
+    def new 
+        @salesman = Salesman.new 
+    end
 end
--- app/models/salesman.rb
+++ app/models/salesman.rb
@@ -2,4 +2,5 @@ class Salesman < ApplicationRecord
     has_many :purchase_orders
     has_many :customers, through: :purchase_orders
     validates :name, presence: :true
+    has_one_attached :avatar
 end
