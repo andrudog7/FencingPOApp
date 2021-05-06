4275d3c824cfaa8bf0e388783901309c60b0be00
--add <andrewksmoker@gmail.com>
2021-05-06 15:09:13 -0400

    adding image upload for Salesman

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -6,4 +6,15 @@ class SalesmenController < ApplicationController
     def new 
         @salesman = Salesman.new 
     end
+
+    def create 
+        @salesman = Salesman.create(salesmen_params)
+        redirect_to salesmen_path
+    end
+
+    private 
+
+    def salesmen_params
+        params.require(:salesman).permit(:name, :avatar)
+    end
 end
