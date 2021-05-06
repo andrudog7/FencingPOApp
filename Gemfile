725cd79f008ee5b89b9cf87739dc867af1da8008
--add <andrewksmoker@gmail.com>
2021-05-06 15:53:37 -0400

    adding thumbnail of image

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'rack-cors'
 gem 'bcrypt', '~> 3.1.7'
 gem 'rspec-rails'
 gem "aws-sdk-s3", require: false
+gem "mini_magick"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/salesman.rb
+++ app/models/salesman.rb
@@ -1,6 +1,17 @@
+
 class Salesman < ApplicationRecord
     has_many :purchase_orders
     has_many :customers, through: :purchase_orders
     validates :name, presence: :true
-    has_one_attached :avatar
+    has_one_attached :avatar do |attachable|
+        attachable.variant :thumb, resize: "100x100"
+    end
+
+    def thumbnail
+        image = Image.open(self.avatar)
+        image.resize "100x100"
+        image.format "png"
+        image.write "output.png"
+        image
+    end
 end
